e5f6fbb787c91bbe4100522178a77053c939fd12
Denz12k <denizkaramankarakartal@hotmail.com>
2025-04-08 14:58:06 +0300

    Game class updated.
    Test objects which suitable for the new game class were created.

--- GameCollection/src/main/java/com/example/gamecollection/Game.java
+++ GameCollection/src/main/java/com/example/gamecollection/Game.java
@@ -1,37 +1,79 @@
 package com.example.gamecollection;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     private String name;
-    private String imagePath;
+    private String developer;
     private int year;
+    private String steamAppId;
+    private List<String> genres;
+    private List<String> publishers;
+    private List<String> platforms;
+    private int totalPlaytimeHours;
+    private String imagePath = "image.jpeg";
+    private List<Localization> localizations;
 
-    public Game(String name, String imagePath, int year) {
-        this.name = name;
-        this.imagePath = imagePath;
-        this.year = year;
-    }
-    public Game() {
-        this.name = "";
-        this.imagePath = "";
-        this.year = 0;
+    public static class Localization {
+        private String language;
+        private List<String> translators;
+        private List<String> dubbingArtists;
+
+        public Localization(String language) {
+            this.language = language;
+            this.translators = new ArrayList<>();
+            this.dubbingArtists = new ArrayList<>();
+        }
+
+        public String getLanguage() { return language; }
+        public List<String> getTranslators() { return translators; }
+        public List<String> getDubbingArtists() { return dubbingArtists; }
+
+        public void addTranslator(String translator) {
+            translators.add(translator);
+        }
+
+        public void addDubbingArtist(String artist) {
+            dubbingArtists.add(artist);
+        }
     }
 
-    public String getName() {
-        return name;
-    }
-    public String getImagePath() {
-        return imagePath;
-    }
-    public int getYear() {
-        return year;
-    }
-    public void setName(String name) {
+    public Game(String name, String developer, int year, String steamAppId) {
         this.name = name;
-    }
-    public void setImagePath(String imagePath) {
-        this.imagePath = imagePath;
-    }
-    public void setYear(int year) {
+        this.developer = developer;
         this.year = year;
+        this.steamAppId = steamAppId;
+        this.genres = new ArrayList<>();
+        this.publishers = new ArrayList<>();
+        this.platforms = new ArrayList<>();
+        this.localizations = new ArrayList<>();
+    }
+
+    public String getName() { return name; }
+    public String getDeveloper() { return developer; }
+    public int getYear() { return year; }
+    public String getSteamAppId() { return steamAppId; }
+    public List<String> getGenres() { return genres; }
+    public List<String> getPublishers() { return publishers; }
+    public List<String> getPlatforms() { return platforms; }
+    public int getTotalPlaytimeHours() { return totalPlaytimeHours; }
+    public String getImagePath() { return imagePath; }
+    public List<Localization> getLocalizations() { return localizations; }
+
+    public void setTotalPlaytimeHours(int hours) { totalPlaytimeHours = hours; }
+    public void setImagePath(String path) { imagePath = path; }
+
+    public void addGenre(String genre) { genres.add(genre); }
+    public void addPublisher(String publisher) { publishers.add(publisher); }
+    public void addPlatform(String platform) { platforms.add(platform); }
+    public void addLocalization(Localization localization) { localizations.add(localization); }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Game: %s (%d)\nDeveloper: %s\nSteam ID: %s\nGenres: %s\nPlaytime: %d hours",
+                name, year, developer, steamAppId, genres, totalPlaytimeHours
+        );
     }
 }
--- GameCollection/src/main/java/com/example/gamecollection/Main.java
+++ GameCollection/src/main/java/com/example/gamecollection/Main.java
@@ -63,45 +63,229 @@ public class Main extends Application {
     }
 
     private void listFiller() {
-        games.add(new Game("League of Legends", "https://upload.wikimedia.org/wikipedia/tr/thumb/7/77/League_of_Legends_logo.png/220px-League_of_Legends_logo.png", 2009));
-        games.add(new Game("Minecraft", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png", 2011));
-        games.add(new Game("Counter Strike GO", "https://upload.wikimedia.org/wikipedia/en/6/6e/CSGOcoverMarch2020.jpg", 2012));
-        games.add(new Game("League of Legends", "https://upload.wikimedia.org/wikipedia/tr/thumb/7/77/League_of_Legends_logo.png/220px-League_of_Legends_logo.png",2009));
-        games.add(new Game("Minecraft", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png",2011));
-        games.add(new Game("Counter Strike Global Offensive", "https://upload.wikimedia.org/wikipedia/en/6/6e/CSGOcoverMarch2020.jpg",2012));
-        games.add(new Game("League of Legends", "https://upload.wikimedia.org/wikipedia/tr/thumb/7/77/League_of_Legends_logo.png/220px-League_of_Legends_logo.png",2009));
-        games.add(new Game("Minecraft", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png",2011));
-        games.add(new Game("Counter Strike Global Offensive", "https://upload.wikimedia.org/wikipedia/en/6/6e/CSGOcoverMarch2020.jpg",2012));
-        games.add(new Game("League of Legends", "https://upload.wikimedia.org/wikipedia/tr/thumb/7/77/League_of_Legends_logo.png/220px-League_of_Legends_logo.png",2009));
-        games.add(new Game("Minecraft", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png",2011));
-        games.add(new Game("Counter Strike Global Offensive", "https://upload.wikimedia.org/wikipedia/en/6/6e/CSGOcoverMarch2020.jpg",2012));
-        games.add(new Game("League of Legends", "https://upload.wikimedia.org/wikipedia/tr/thumb/7/77/League_of_Legends_logo.png/220px-League_of_Legends_logo.png",2009));
-        games.add(new Game("Minecraft", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png",2011));
-        games.add(new Game("Counter Strike Global Offensive", "https://upload.wikimedia.org/wikipedia/en/6/6e/CSGOcoverMarch2020.jpg",2012));
-        games.add(new Game("League of Legends", "https://upload.wikimedia.org/wikipedia/tr/thumb/7/77/League_of_Legends_logo.png/220px-League_of_Legends_logo.png",2009));
-        games.add(new Game("Minecraft", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png",2011));
-        games.add(new Game("Counter Strike Global Offensive", "https://upload.wikimedia.org/wikipedia/en/6/6e/CSGOcoverMarch2020.jpg",2012));
-        games.add(new Game("League of Legends", "https://upload.wikimedia.org/wikipedia/tr/thumb/7/77/League_of_Legends_logo.png/220px-League_of_Legends_logo.png",2009));
-        games.add(new Game("Minecraft", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png",2011));
-        games.add(new Game("Counter Strike Global Offensive", "https://upload.wikimedia.org/wikipedia/en/6/6e/CSGOcoverMarch2020.jpg",2012));
-        games.add(new Game("League of Legends", "https://upload.wikimedia.org/wikipedia/tr/thumb/7/77/League_of_Legends_logo.png/220px-League_of_Legends_logo.png",2009));
-        games.add(new Game("Minecraft", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png",2011));
-        games.add(new Game("Counter Strike Global Offensive", "https://upload.wikimedia.org/wikipedia/en/6/6e/CSGOcoverMarch2020.jpg",2012));
-        games.add(new Game("League of Legends", "https://upload.wikimedia.org/wikipedia/tr/thumb/7/77/League_of_Legends_logo.png/220px-League_of_Legends_logo.png",2009));
-        games.add(new Game("Minecraft", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png",2011));
-        games.add(new Game("Counter Strike Global Offensive", "https://upload.wikimedia.org/wikipedia/en/6/6e/CSGOcoverMarch2020.jpg",2012));
-        games.add(new Game("League of Legends", "https://upload.wikimedia.org/wikipedia/tr/thumb/7/77/League_of_Legends_logo.png/220px-League_of_Legends_logo.png",2009));
-        games.add(new Game("Minecraft", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png",2011));
-        games.add(new Game("Counter Strike Global Offensive", "https://upload.wikimedia.org/wikipedia/en/6/6e/CSGOcoverMarch2020.jpg",2012));
-        games.add(new Game("League of Legends", "https://upload.wikimedia.org/wikipedia/tr/thumb/7/77/League_of_Legends_logo.png/220px-League_of_Legends_logo.png",2009));
-        games.add(new Game("Minecraft", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png",2011));
-        games.add(new Game("Counter Strike Global Offensive", "https://upload.wikimedia.org/wikipedia/en/6/6e/CSGOcoverMarch2020.jpg",2012));
-        games.add(new Game("League of Legends", "https://upload.wikimedia.org/wikipedia/tr/thumb/7/77/League_of_Legends_logo.png/220px-League_of_Legends_logo.png",2009));
-        games.add(new Game("Minecraft", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png",2011));
-        games.add(new Game("Counter Strike Global Offensive", "https://upload.wikimedia.org/wikipedia/en/6/6e/CSGOcoverMarch2020.jpg",2012));
-        games.add(new Game("League of Legends", "https://upload.wikimedia.org/wikipedia/tr/thumb/7/77/League_of_Legends_logo.png/220px-League_of_Legends_logo.png",2009));
-        games.add(new Game("Minecraft", "https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png",2011));
-        games.add(new Game("Counter Strike Global Offensive", "https://upload.wikimedia.org/wikipedia/en/6/6e/CSGOcoverMarch2020.jpg",2012));
+        Game witcher3 = new Game("The Witcher 3: Wild Hunt", "CD Projekt Red", 2015, "292030");
+        witcher3.addGenre("RPG");
+        witcher3.addGenre("Open World");
+        witcher3.addPublisher("CD Projekt");
+        witcher3.addPlatform("PC");
+        witcher3.addPlatform("PlayStation 4");
+        witcher3.addPlatform("Xbox One");
+        witcher3.setTotalPlaytimeHours(150);
+        witcher3.setImagePath("https://upload.wikimedia.org/wikipedia/en/thumb/0/0c/Witcher_3_cover_art.jpg/250px-Witcher_3_cover_art.jpg");
+        games.add(witcher3);
+
+        Game lol = new Game("League of Legends", "Riot Games", 2009, "123456");
+        lol.addGenre("MOBA");
+        lol.addGenre("Strategy");
+        lol.addPublisher("Riot Games");
+        lol.addPlatform("PC");
+        lol.setTotalPlaytimeHours(500);
+        lol.setImagePath("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/League_of_Legends_2019_vector.svg/250px-League_of_Legends_2019_vector.svg.png");
+        games.add(lol);
+
+        Game cs2 = new Game("Counter-Strike 2", "Valve", 2023, "234567");
+        cs2.addGenre("FPS");
+        cs2.addGenre("Shooter");
+        cs2.addPublisher("Valve");
+        cs2.addPlatform("PC");
+        cs2.setTotalPlaytimeHours(300);
+        cs2.setImagePath("https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/CS2_Cover_Art.jpg/220px-CS2_Cover_Art.jpg");
+        games.add(cs2);
+
+        Game valorant = new Game("Valorant", "Riot Games", 2020, "345678");
+        valorant.addGenre("FPS");
+        valorant.addGenre("Tactical Shooter");
+        valorant.addPublisher("Riot Games");
+        valorant.addPlatform("PC");
+        valorant.setTotalPlaytimeHours(250);
+        valorant.setImagePath("https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Valorant_logo_-_pink_color_version.svg/220px-Valorant_logo_-_pink_color_version.svg.png");
+        games.add(valorant);
+
+        Game minecraft = new Game("Minecraft", "Mojang Studios", 2011, "456789");
+        minecraft.addGenre("Sandbox");
+        minecraft.addGenre("Adventure");
+        minecraft.addPublisher("Xbox Game Studios");
+        minecraft.addPlatform("PC");
+        minecraft.addPlatform("PlayStation");
+        minecraft.addPlatform("Xbox");
+        minecraft.addPlatform("Nintendo Switch");
+        minecraft.addPlatform("Mobile");
+        minecraft.setTotalPlaytimeHours(1000);
+        minecraft.setImagePath("https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png");
+        games.add(minecraft);
+
+        Game amongUs = new Game("Among Us", "InnerSloth", 2018, "567890");
+        amongUs.addGenre("Party");
+        amongUs.addGenre("Social Deduction");
+        amongUs.addPublisher("InnerSloth");
+        amongUs.addPlatform("PC");
+        amongUs.addPlatform("Mobile");
+        amongUs.addPlatform("Nintendo Switch");
+        amongUs.addPlatform("PlayStation");
+        amongUs.addPlatform("Xbox");
+        amongUs.setTotalPlaytimeHours(50);
+        amongUs.setImagePath("https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Among_Us_cover_art.jpg/250px-Among_Us_cover_art.jpg");
+        games.add(amongUs);
+
+        Game pubg = new Game("PUBG: Battlegrounds", "PUBG Corporation", 2017, "678901");
+        pubg.addGenre("Battle Royale");
+        pubg.addGenre("Shooter");
+        pubg.addPublisher("Krafton");
+        pubg.addPlatform("PC");
+        pubg.addPlatform("PlayStation");
+        pubg.addPlatform("Xbox");
+        pubg.addPlatform("Mobile");
+        pubg.setTotalPlaytimeHours(200);
+        pubg.setImagePath("https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Pubgbattlegrounds.png/250px-Pubgbattlegrounds.png");
+        games.add(pubg);
+
+        Game cyberpunk = new Game("Cyberpunk 2077", "CD Projekt Red", 2020, "789012");
+        cyberpunk.addGenre("RPG");
+        cyberpunk.addGenre("FPS");
+        cyberpunk.addGenre("Open World");
+        cyberpunk.addPublisher("CD Projekt");
+        cyberpunk.addPlatform("PC");
+        cyberpunk.addPlatform("PlayStation 5");
+        cyberpunk.addPlatform("Xbox Series X/S");
+        cyberpunk.setTotalPlaytimeHours(80);
+        cyberpunk.setImagePath("https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Cyberpunk_2077_box_art.jpg/250px-Cyberpunk_2077_box_art.jpg");
+        games.add(cyberpunk);
+
+        Game eldenRing = new Game("Elden Ring", "FromSoftware", 2022, "890123");
+        eldenRing.addGenre("Action RPG");
+        eldenRing.addGenre("Open World");
+        eldenRing.addPublisher("Bandai Namco");
+        eldenRing.addPublisher("FromSoftware");
+        eldenRing.addPlatform("PC");
+        eldenRing.addPlatform("PlayStation 5");
+        eldenRing.addPlatform("PlayStation 4");
+        eldenRing.addPlatform("Xbox Series X/S");
+        eldenRing.addPlatform("Xbox One");
+        eldenRing.setTotalPlaytimeHours(120);
+        eldenRing.setImagePath("https://upload.wikimedia.org/wikipedia/en/thumb/b/b9/Elden_Ring_Box_art.jpg/250px-Elden_Ring_Box_art.jpg");
+        games.add(eldenRing);
+
+        Game.Localization englishLoc = new Game.Localization("English");
+        englishLoc.addTranslator("Ryan Morris");
+        englishLoc.addTranslator("Emily Johnson");
+        englishLoc.addDubbingArtist("Paul Bandey"); // Godrick seslendirmen
+        englishLoc.addDubbingArtist("Lindsay Seidel"); // Melina seslendirmen
+
+        Game.Localization turkishLoc = new Game.Localization("Turkish");
+        turkishLoc.addTranslator("Mehmet Can Korkut");
+        turkishLoc.addTranslator("Ayşe Demir");
+        turkishLoc.addDubbingArtist("Sercan Gidiş"); // Godrick seslendirmen
+        turkishLoc.addDubbingArtist("Zeynep Taşkın"); // Melina seslendirmen
+
+        Game.Localization japaneseLoc = new Game.Localization("Japanese");
+        japaneseLoc.addTranslator("Hiroshi Yamamoto");
+        japaneseLoc.addDubbingArtist("Kōichi Yamadera"); // Godrick seslendirmen
+
+        eldenRing.addLocalization(englishLoc);
+        eldenRing.addLocalization(turkishLoc);
+        eldenRing.addLocalization(japaneseLoc);
+
+        Game witcher3x = new Game("The Witcher 3: Wild Hunt", "CD Projekt Red", 2015, "292030");
+        witcher3x.addGenre("RPG");
+        witcher3x.addGenre("Open World");
+        witcher3x.addPublisher("CD Projekt");
+        witcher3x.addPlatform("PC");
+        witcher3x.addPlatform("PlayStation 4");
+        witcher3x.addPlatform("Xbox One");
+        witcher3x.setTotalPlaytimeHours(150);
+        witcher3x.setImagePath("https://upload.wikimedia.org/wikipedia/en/thumb/0/0c/Witcher_3_cover_art.jpg/250px-Witcher_3_cover_art.jpg");
+        games.add(witcher3x);
+
+        Game lol1 = new Game("League of Legends", "Riot Games", 2009, "123456");
+        lol1.addGenre("MOBA");
+        lol1.addGenre("Strategy");
+        lol1.addPublisher("Riot Games");
+        lol1.addPlatform("PC");
+        lol1.setTotalPlaytimeHours(500);
+        //lol1.setImagePath("https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/League_of_Legends_2019_vector.svg/250px-League_of_Legends_2019_vector.svg.png");
+        games.add(lol1);
+
+        Game cs2x = new Game("Counter-Strike 2", "Valve", 2023, "234567");
+        cs2x.addGenre("FPS");
+        cs2x.addGenre("Shooter");
+        cs2x.addPublisher("Valve");
+        cs2x.addPlatform("PC");
+        cs2x.setTotalPlaytimeHours(300);
+        cs2x.setImagePath("https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/CS2_Cover_Art.jpg/220px-CS2_Cover_Art.jpg");
+        games.add(cs2x);
+
+        Game valorant1 = new Game("Valorant", "Riot Games", 2020, "345678");
+        valorant1.addGenre("FPS");
+        valorant1.addGenre("Tactical Shooter");
+        valorant1.addPublisher("Riot Games");
+        valorant1.addPlatform("PC");
+        valorant1.setTotalPlaytimeHours(250);
+        //valorant1.setImagePath("https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Valorant_logo_-_pink_color_version.svg/220px-Valorant_logo_-_pink_color_version.svg.png");
+        games.add(valorant1);
+
+        Game minecraft1 = new Game("Minecraft", "Mojang Studios", 2011, "456789");
+        minecraft1.addGenre("Sandbox");
+        minecraft1.addGenre("Adventure");
+        minecraft1.addPublisher("Xbox Game Studios");
+        minecraft1.addPlatform("PC");
+        minecraft1.addPlatform("PlayStation");
+        minecraft1.addPlatform("Xbox");
+        minecraft1.addPlatform("Nintendo Switch");
+        minecraft1.addPlatform("Mobile");
+        minecraft1.setTotalPlaytimeHours(1000);
+        minecraft1.setImagePath("https://upload.wikimedia.org/wikipedia/en/thumb/b/b6/Minecraft_2024_cover_art.png/250px-Minecraft_2024_cover_art.png");
+        games.add(minecraft1);
+
+        Game amongUs1 = new Game("Among Us", "InnerSloth", 2018, "567890");
+        amongUs1.addGenre("Party");
+        amongUs1.addGenre("Social Deduction");
+        amongUs1.addPublisher("InnerSloth");
+        amongUs1.addPlatform("PC");
+        amongUs1.addPlatform("Mobile");
+        amongUs1.addPlatform("Nintendo Switch");
+        amongUs1.addPlatform("PlayStation");
+        amongUs1.addPlatform("Xbox");
+        amongUs1.setTotalPlaytimeHours(50);
+        amongUs1.setImagePath("https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Among_Us_cover_art.jpg/250px-Among_Us_cover_art.jpg");
+        games.add(amongUs1);
+
+        Game pubg1 = new Game("PUBG: Battlegrounds", "PUBG Corporation", 2017, "678901");
+        pubg1.addGenre("Battle Royale");
+        pubg1.addGenre("Shooter");
+        pubg1.addPublisher("Krafton");
+        pubg1.addPlatform("PC");
+        pubg1.addPlatform("PlayStation");
+        pubg1.addPlatform("Xbox");
+        pubg1.addPlatform("Mobile");
+        pubg1.setTotalPlaytimeHours(200);
+        pubg1.setImagePath("https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Pubgbattlegrounds.png/250px-Pubgbattlegrounds.png");
+        games.add(pubg1);
+
+        Game cyberpunk1 = new Game("Cyberpunk 2077", "CD Projekt Red", 2020, "789012");
+        cyberpunk1.addGenre("RPG");
+        cyberpunk1.addGenre("FPS");
+        cyberpunk1.addGenre("Open World");
+        cyberpunk1.addPublisher("CD Projekt");
+        cyberpunk1.addPlatform("PC");
+        cyberpunk1.addPlatform("PlayStation 5");
+        cyberpunk1.addPlatform("Xbox Series X/S");
+        cyberpunk1.setTotalPlaytimeHours(80);
+        cyberpunk1.setImagePath("https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Cyberpunk_2077_box_art.jpg/250px-Cyberpunk_2077_box_art.jpg");
+        games.add(cyberpunk1);
+
+        Game eldenRing1 = new Game("Elden Ring", "FromSoftware", 2022, "890123");
+        eldenRing1.addGenre("Action RPG");
+        eldenRing1.addGenre("Open World");
+        eldenRing1.addPublisher("Bandai Namco");
+        eldenRing1.addPublisher("FromSoftware");
+        eldenRing1.addPlatform("PC");
+        eldenRing1.addPlatform("PlayStation 5");
+        eldenRing1.addPlatform("PlayStation 4");
+        eldenRing1.addPlatform("Xbox Series X/S");
+        eldenRing1.addPlatform("Xbox One");
+        eldenRing1.setTotalPlaytimeHours(120);
+        eldenRing1.setImagePath("https://upload.wikimedia.org/wikipedia/en/thumb/b/b9/Elden_Ring_Box_art.jpg/250px-Elden_Ring_Box_art.jpg");
+        games.add(eldenRing1);
     }
 
     private HBox bottomBox() {
